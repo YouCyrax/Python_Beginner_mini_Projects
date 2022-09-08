9ce8fd6ee21ff3deba7d4a31f4b67fc6d9d3ef93
Younes <seghirou.younes@gmail.com>
2022-09-08 22:24:36 +0100

    Add madlib Python code

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+adj = input("Adjective: ")
+verb1 = input("Verb 1:  ")
+verb2 = input("Verb 2:  ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! it makes me so excited all the time because i love to {verb1} .Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
